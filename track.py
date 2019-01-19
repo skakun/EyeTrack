7b7dbb944cdbb8c3b84cc5971f3538af49e129f4
skakun <krzysztof.michalski.it>
2019-01-19 21:07:50 +0100

    init

---
+++ track.py
@@ -0,0 +1,43 @@
+from imutils import face_utils
+import numpy as np
+import pyautogui as pag
+import imutils
+import dlib
+import cv2
+import time
+
+capture = cv2.VideoCapture(0)
+
+shape_predictor = "shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(shape_predictor)
+YELLOW_COLOR = (0, 255, 255)
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+def main():
+	while True:
+		_, frame = capture.read()
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		rects=detector(gray,1)
+		if len(rects) > 0:
+			rect = rects[0]
+		else:
+			cv2.imshow("Frame", frame)
+			key = cv2.waitKey(1) & 0xFF
+			continue
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		leftEyeHull = cv2.convexHull(leftEye)
+		rightEyeHull = cv2.convexHull(rightEye)
+
+		cv2.drawContours(frame, [leftEyeHull], -1, YELLOW_COLOR, 1)
+		cv2.drawContours(frame, [rightEyeHull], -1, YELLOW_COLOR, 1)
+		cv2.imshow("Frame", frame)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+
+main()
