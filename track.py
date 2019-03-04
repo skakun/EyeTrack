1a711d418b6af4038f75de791ea40839a8bcd95d
skakun <krzysztof.michalski.it>
2019-03-04 22:01:44 +0100

    Calculating eye aspect, propably something wrong

--- track.py
+++ track.py
@@ -5,16 +5,16 @@ import imutils
 import dlib
 import cv2
 import time
+from scipy.spatial import distance 
 
 radius=5
 
 class eyeSnip():
 	def __init__(self,frame,hull,rl):
+		self.left_or_right=rl
 		if rl=='r':
-			self.snip,self.shiftbox=eyeSnip.reye_box(hull, frame)
+			self.snip,self.shiftbox,self.eye_aspect_ratio=eyeSnip.reye_box(hull, frame)
 		self.scope=self.shiftbox["maxx"]-self.shiftbox["minx"],self.shiftbox["maxy"]-self.shiftbox["miny"]
-#	print("{}:\n".format(rl))
-#	print("Eye scope:/n x: {} /n y:{}".format(scope[0],scope[1]))
 		self.scope_OK=not (self.scope[0]<=0 or self.scope[1]<=0)
 	def calc_darkest_point(self):
 		blur_snip=cv2.cvtColor(self.snip,cv2.COLOR_BGR2GRAY)
@@ -30,19 +30,20 @@ class eyeSnip():
 		miny=int((shape[38][1]+shape[39][1])/2)
 		minx=shape[37][0]
 		maxx=shape[40][0]
-		marginx=1*(maxx-minx)
-		marginy=1*(maxy-miny)
-		########maxy+=marginy
-		########miny-=marginy
-		########maxx+=marginx
-		########minx-=marginx
+		eye_aspect_ratio=( distance.euclidean(shape[42],shape[38])+distance.euclidean(shape[39],shape[41]))/(2*distance.euclidean(shape[37],shape[40]))
+		marginx=int(1*(maxx-minx))
+		marginy=int(1*(maxy-miny))
+		maxy+=marginy
+		miny-=marginy
+		maxx+=marginx
+		minx-=marginx
 		shiftbox={
 			"minx":minx,
 			"maxx":maxx,
 			"miny":miny,
 			"maxy":maxy
 		}
-		return frame[miny:maxy,minx:maxx],shiftbox
+		return frame[miny:maxy,minx:maxx],shiftbox,eye_aspect_ratio
 
 shape_predictor = "shape_predictor_68_face_landmarks.dat"
 detector = dlib.get_frontal_face_detector()
@@ -51,20 +52,6 @@ YELLOW_COLOR = (0, 255, 255)
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
-########def leyeBox(shape, frame):
-########	miny=int((shape[44][1]+shape[45][1])/2)
-########	maxy=int((shape[48][1]+shape[47][1])/2)
-########	minx=shape[43][0]
-########	maxx=shape[46][0]
-########	marginx=(maxx-minx)
-########	marginy=(maxy-miny)
-########	maxy+=marginy
-########	miny-=marginy
-########	maxx+=marginx
-########	minx-=marginx
-########	print("leye\n")
-########	print("miny: {} \n maxy: {} \n minx: {} \n maxx: {} \n".format(miny,maxy,minx,maxx))
-########	return frame[miny:maxy,minx:maxx]
 def transPoint(point,oldScope,newScope,resc=(1,1)):
 	return (int(resc[0]*point[0]/oldScope[0]*newScope[0]) ,int(resc[1]*point[1]/oldScope[1]*newScope[1]))
 
@@ -93,6 +80,7 @@ def main():
 		#cv2.imshow("Frame",frame)		
 			cv2.waitKey(1)
 			continue
+		print("Right eye:\n Retina pos in frame: {} \n Retina pos in snip: {}\n Ear:{}".format(reye.calc_shifted_darkest_point(),reye.calc_darkest_point(),reye.eye_aspect_ratio ))
 		marked_reye=reye.snip
 		cv2.circle(marked_reye,reye.calc_darkest_point(),radius, (0, 255, 0), 2)
 		cv2.imshow("reye",marked_reye)
@@ -108,29 +96,3 @@ def main():
 		cv2.imshow("Screenshot", sshot)
 		cv2.waitKey(1)
 main()
-#	gframe=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#	gframe = cv2.GaussianBlur(gframe, (radius,radius), 0)
-#	(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gframe)
-#	(reye,rshiftbox)=eyeSnip.reye_box(shape, frame)
-#	rscope=rshiftbox["maxx"]-rshiftbox["minx"],rshiftbox["maxy"]-rshiftbox["miny"]
-#	print("Reye scope:/n x: {} /n y:{}".format(rscope[0],rscope[1]))
-#	if rscope[0]<=0 or rscope[1]<=0:
-##	cv2.imshow("Frame", frame)
-#(rminLoc[0] + rshiftbox["minx"], rminLoc[1] + rshiftbox["miny"])			cv2.waitKey(1)
-			#continue
-		#breye=cv2.cvtColor(reye, cv2.COLOR_BGR2GRAY)
-		#breye=cv2.GaussianBlur(breye, (radius,radius), 0)
-
-		#(rminVal, rmaxVal, rminLoc, rmaxLoc) = cv2.minMaxLoc(breye)
-		#rshiftedLoc=(rminLoc[0]+rshiftbox["minx"],rminLoc[1]+rshiftbox["miny"])
-		#print("rminloc:\n \t within eye: {}\n within frame \n \t {}".format(rminLoc,rshiftedLoc))
-		#cv2.circle(reye, rminLoc,radius, (0, 255, 0), 2)
-		#cv2.imshow("reye",reye)
-		#cv2.drawContours(frame, [leftEyeHull], -1, YELLOW_COLOR, 1)
-		#cv2.drawContours(frame, [rightEyeHull], -1, YELLOW_COLOR, 1)
-		#cv2.circle(frame, rshiftedLoc,radius, (0, 255, 0), 2)
-		#cv2.imshow("Frame", frame)
-		#sshot=cv2.imread('idylla.jpg',0)
-		#cursorPos=transPoint(rminLoc,rscope,sshot.shape[:2],(1,1))
-		#print("Rescaled right retina pos to frame:\n x:{}\ny:{}".format(cursorPos[0],cursorPos[1]))
-	##sshot = pag.screenshot()
