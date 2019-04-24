dca740e81ae2a7e6becbe55d26282852107c5066
Maciej Tarnowski <maciek_tarnowski@yahoo.pl>
2019-04-24 22:42:52 +0200

    segmentation testing

--- track.py
+++ track.py
@@ -7,65 +7,30 @@ import cv2
 import time
 import math
 from scipy.spatial import distance
+
 radius = 5
 WIDTH, HEIGHT = 640, 480
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+n = 0
+
+
 class EyeSnipper:
     @staticmethod
-    def eye_box_hull(frame, shape, side):
-        minx, maxx, miny, maxy, eye_aspect_ratio = 0, 0, 0, 0, 0
-        if side == 'r':
-            miny = shape[37][1] if shape[37][1] < shape[38][1] else shape[38][1]
-            maxy = shape[40][1] if shape[40][1] > shape[41][1] else shape[41][1]
-            minx = shape[36][0]
-            maxx = shape[39][0]
-            eye_aspect_ratio = (distance.euclidean(shape[37], shape[41]) + distance.euclidean(shape[38], shape[40])) / (
-                                2 * distance.euclidean(shape[36], shape[39]))
-        elif side == 'l':
-            miny = shape[43][1] if shape[43][1] < shape[44][1] else shape[44][1]
-            maxy = shape[46][1] if shape[46][1] > shape[47][1] else shape[47][1]
-            minx = shape[42][0]
-            maxx = shape[45][0]
-            eye_aspect_ratio = (distance.euclidean(shape[43], shape[47]) + distance.euclidean(shape[44], shape[46])) / (
-                                2 * distance.euclidean(shape[42], shape[45]))
+    def get_from_haar(frame, cascade):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        eyes = cascade.detectMultiScale(gray)
+        if len(eyes) == 0:
+            return None, False
 
-        marginx = int(0.1 * (maxx - minx))
-        marginy = int(0.1 * (maxy - miny))
-    #   minx -= marginx
-    #   maxx += marginx
-     #  maxy += marginy
-     #  miny -= marginy
-        shiftbox = {
-            "minx": minx,
-            "maxx": maxx,
-            "miny": miny,
-            "maxy": maxy
-        }
-        return frame[miny:maxy, minx:maxx], shiftbox, eye_aspect_ratio
-    @staticmethod
-    def get_from_hull(frame,shape,side):
-        cropped_frame,shiftbox,ear=EyeSnipper.eye_box_hull(frame,shape,side)
-        scope= shiftbox["maxx"] - shiftbox["minx"], shiftbox["maxy"] - shiftbox["miny"]
-        snip=EyeSnip(cropped_frame,side,shiftbox,scope)
-        snip.check_scope()
-        snip.shiftbox_OK=True
-        snip.eye_aspect_ratio=1
-        return snip
-    @staticmethod
-    def get_from_haar(frame,cascade):
-        gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        eyes=cascade.detectMultiScale(gray)
-        if len(eyes)==0:
-            return None,False
-        eye=eyes[0]
-        minx=eye[0]
-        maxx=eye[0]+eye[2]
-        miny=eye[1]
-        maxy=eye[1]+eye[3]
+        eye = eyes[0]
+        minx = eye[0]
+        maxx = eye[0] + eye[2]
+        miny = eye[1]
+        maxy = eye[1] + eye[3]
         marginx = int(0.2 * (maxx - minx))
         marginy = int(0.2 * (maxy - miny))
-   #    minx += marginx
-   #    maxx -= marginx
+        #    minx += marginx
+        #    maxx -= marginx
         maxy -= marginy
         miny += marginy
         shiftbox = {
@@ -74,91 +39,75 @@ class EyeSnipper:
             "miny": miny,
             "maxy": maxy
         }
-        cropped_frame= frame[miny:maxy, minx:maxx]
-        scope= shiftbox["maxx"] - shiftbox["minx"], shiftbox["maxy"] - shiftbox["miny"]
-        side='r'
-        snip=EyeSnip(cropped_frame,side,shiftbox,scope)
+        cropped_frame = frame[miny:maxy, minx:maxx]
+        scope = shiftbox["maxx"] - shiftbox["minx"], shiftbox["maxy"] - shiftbox["miny"]
+        side = 'r'
+        snip = EyeSnip(cropped_frame, side, shiftbox, scope)
         snip.check_scope()
-        snip.shiftbox_OK=True
-        snip.eye_aspect_ratio=1
-        return snip,True
+        snip.shiftbox_OK = True
+        snip.eye_aspect_ratio = 1
+        return snip, True
+
+
 class EyeSnip:
-    def __init__(self,snip,side,shiftbox,scope):
-        self.snip=snip
-        self.side=side
-        self.scope=scope
-        self.shiftbox=shiftbox
-        self.gray_snip= cv2.cvtColor(self.snip,cv2.COLOR_BGR2GRAY)
+    def __init__(self, snip, side, shiftbox, scope):
+        self.snip = snip
+        self.side = side
+        self.scope = scope
+        self.shiftbox = shiftbox
+        self.gray_snip = cv2.cvtColor(self.snip, cv2.COLOR_BGR2GRAY)
+        self.segmented = self.get_segments()
+
     def check_scope(self):
-        self.scope_OK=not (self.scope[0] <= 0 or self.scope[1] <= 0)
+        self.scope_OK = not (self.scope[0] <= 0 or self.scope[1] <= 0)
         return self.scope_OK
-    def get_countur(self):
-        blurred = cv2.GaussianBlur(self.gray_snip, (5, 5), 0)
-        thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY_INV)[1]
-        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        image=self.snip.copy()
-        for c in cnts:
-        # compute the center of the contour
-            M = cv2.moments(c)
-            if M["m00"]==0:
-                continue
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-            cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-       #cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
-        cv2.putText(image, "center", (cX - 20, cY - 20),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-        return image
-    def calc_darkest_point(self):
-        blur_snip = cv2.cvtColor(self.snip, cv2.COLOR_BGR2GRAY)
-        blur_snip = cv2.GaussianBlur(blur_snip, (radius, radius), 0)
-        (min_val, max_val, min_loc, max_loc) = cv2.minMaxLoc(blur_snip)
-        return min_loc
-
-    def calc_shifted_darkest_point(self):
-        min_loc = self.calc_darkest_point()
-        return min_loc[0] + self.shiftbox["minx"], min_loc[1] + self.shiftbox["miny"]
-
-    def canny_edges(self):
-        blur_snip = cv2.cvtColor(self.snip, cv2.COLOR_BGR2GRAY)
-        blur_snip = cv2.GaussianBlur(blur_snip, (radius, radius), 0)
-        low_threshold = 35
-        high_threshold = low_threshold * 3
-        eye_edges = cv2.Canny(blur_snip, low_threshold, high_threshold)
-        return eye_edges
-
-    def get_thresh(self):
-        ret, thresh = cv2.threshold(self.gray_snip,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        return thresh
-    def get_blur_thresh(self):
-   #    blurred = cv2.GaussianBlur(self.gray_snip, (5, 5), 0)
-        blurred = cv2.GaussianBlur(self.snip, (5, 5), 0)
-        ret, thresh = cv2.threshold(blurred,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        return thresh
 
     def get_segments(self):
-        im=self.snip.copy()
-        blurred= cv2.GaussianBlur(im, (5, 5), 0)
+        #im = self.snip.copy()
+        gray = cv2.cvtColor(self.snip, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (3, 3), 0)  # można popróbować z różnymi blurami, ale prędzej mniejszy, niż większy wg mnie
 
-        params=cv2.SimpleBlobDetector_Params()
-        params.filterByCircularity=True
-        params.minCircularity=0.3
-        params.filterByColor=True
-        params.blobColor=0
-        detector=cv2.SimpleBlobDetector_create(params)
-        keyPoints=detector.detect(blurred)
+        #im = cv2.equalizeHist(blurred)  # bardziej uniwersalny, bo rozciąga zakres szarości do stałych granic,
+                                         # ale za to trudniej wyróżnić źrenicę na tle tęczówki (dla ciemnych oczu)
+
+        #ret, im = cv2.threshold(blurred, 5, 255, cv2.THRESH_BINARY)  # chamskie thresholdowanie raczej nie działa, ale można coś pokombonować
+        im = blurred
+
+        params = cv2.SimpleBlobDetector_Params()
+        params.minThreshold = 0
+        params.maxThreshold = 45  # albo 15-30 z użyciem equalizeHist albo trochę więcej bez niego (bez niego można lepiej
+                                  # wyizolować źrenicę od tęczówki, ale trzeba uważać na przypadki, gdy w jakimś
+                                  # ultraświetle źrenica byłaby jaśniejsza od tych powiedzmy 30 ustawionych jako maxThreshold
+
+        params.thresholdStep = 5  # thresholduje w górę (bierze wszystko od danego thresholda do 255) od 'min' do 'max' co 'step';
+                                  # blob musi się znajdować przynajmniej w 2 z tych obrazów binarnych (po thresholdzie), aby był brany pod uwagę
+
+        #params.minRepeatability  # ilość obrazów binarnych po segmentacji, w jakich musi się znajdować blob, żeby był brany pod uwagę; nie ruszałem
+
+        #params.minInertiaRatio  # płaskość czy coś, to jest defaultowo ustawione na min 0.1 (chyba stosunek wysokości
+                                 # do szerokości albo coś takiego); nie ruszałem
+
+        #params.filterByCircularity = True
+        #params.minCircularity = 0.3
+        params.filterByConvexity = True
+        params.minConvexity = 0.7  # wypukłość; musi być nie za duża, ale największa możliwa
+        params.filterByArea = True
+        params.minArea = 700  # raczej można jeszcze spokojnie zwiększyć
+        detector = cv2.SimpleBlobDetector_create(params)
+        keyPoints = detector.detect(im)
+        global n
+        if keyPoints:
+            n = 0
+        else:
+            n += 1
         for keypoint in keyPoints:
             x = int(keypoint.pt[0])
             y = int(keypoint.pt[1])
             s = keypoint.size
-            r = int(math.floor(s/2))
-         #  print x, y
+            r = int(math.floor(s / 2))
+            #  print x, y
             cv2.circle(im, (x, y), r, (255, 255, 0), 2)
-        return  im
-#def segment_edges(self):
-#       img=self.canny_edges()
+        return im
 
 
 shape_predictor = "shape_predictor_68_face_landmarks.dat"
@@ -170,53 +119,42 @@ YELLOW_COLOR = (0, 255, 255)
 (rstart, rend) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
 
-def trans_point(point, old_scope, new_scope, resc=(1, 1)):
-    return int(resc[0] * point[0] / old_scope[0] * new_scope[0]), int(resc[1] * point[1] / old_scope[1] * new_scope[1])
-
-
-def cursor_position(event, x, y, flags, param):
+def color_at_cursor_position(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         print('x: ' + str(x))
         print('y: ' + str(y))
+        print(param[y, x])
 
 
 def main():
-    capture = cv2.VideoCapture(0)
-
+    # capture = cv2.VideoCapture(0)
+    capture = cv2.VideoCapture('77.mp4')
     capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-    # set click coordinates helper #
-#   cv2.namedWindow('Frame')
-#   cv2.setMouseCallback('Frame', cursor_position)
-
-    # begin_t = time.time()
-    while True:
+    while capture.isOpened():
         # print("Iteration time: {}".format(time.time()-begin_t))
 
         # find face and eyes #
-        _, frame = capture.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        reye,OK=EyeSnipper.get_from_haar(frame,eye_cascade)
+        capture.read()
+        ret, frame = capture.read()
+        if not ret:
+            break
+        reye, OK = EyeSnipper.get_from_haar(frame, eye_cascade)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
         if not OK:
-            cv2.imshow('frame',frame)
-            cv2.waitKey(1)
             continue
-        print("Right eye:\n Retina pos in frame: {} \n Retina pos in snip: {}\n Ear:{}".format(
-            reye.calc_shifted_darkest_point(), reye.calc_darkest_point(), reye.eye_aspect_ratio))
-
-    #   cv2.circle(frame,reye.calc_shifted_darkest_point(),radius,(0, 255, 0))
 
         # display resized right eye in gray #
-        cv2.imshow("frame", frame)
-        cv2.waitKey(1)
-        cv2.imshow("reye",reye.snip)
-        cv2.waitKey(1)
-        if cv2.waitKey(1) == ord('q'):
-            break
-        cv2.imshow("segments",reye.get_segments())
-        cv2.waitKey(1)
-#       cv2.imshow("contour",reye.get_countur())
+        cv2.imshow("reye", reye.snip)
+
+        cv2.namedWindow("segments", cv2.WINDOW_AUTOSIZE)
+        cv2.setMouseCallback("segments", color_at_cursor_position, param=reye.segmented)
+        cv2.imshow("segments", reye.segmented)
+
 
 if __name__ == '__main__':
     main()
+
